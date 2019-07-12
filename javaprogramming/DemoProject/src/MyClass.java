a8743205aabdafd89d4d85600f7c00096f082b02
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-07-12 15:53:44 -0500

    w3 schools exercises

---
+++ javaprogramming/DemoProject/src/MyClass.java
@@ -0,0 +1,36 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is single-line comment before a line of code
+		System.out.println("Hello World");
+		//This is single-line comment at the end of a line of code
+		/* 
+		 Multi-Line Comments 
+		 */
+		
+		String firstName="sai varun";
+		String lastName= "tangalla";
+		String fullname= firstName +" " +lastName;
+		System.out.println("a");
+		int b = 6;
+		System.out.println(b);
+		int c;
+		c=5;
+		System.out.println(c);
+		System.out.println(firstName+5);
+		System.out.println(fullname);
+		int x=5,y=7,z=8;
+		System.out.println(x+y+z);
+		//create a byte type
+		byte myNum= 100;
+		System.out.println(myNum);
+		//create a short type
+		short myNum2=1000;
+		System.out.println(myNum2);
+		int myNum3=100000;
+		
+	}
+
+}
