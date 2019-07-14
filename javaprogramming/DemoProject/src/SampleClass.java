a4126c50647d6472f9fc73818411c649378df26f
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-07-14 15:13:04 -0500

    Looping through multidimesional arrays:)

--- javaprogramming/DemoProject/src/SampleClass.java
+++ javaprogramming/DemoProject/src/SampleClass.java
@@ -4,6 +4,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/*
 		//Create and access an array
 		int array[]= {1,2,3,6,9,10};
 		
@@ -18,9 +19,11 @@ public class SampleClass {
 		System.out.println(array.length);
 		
 		//Access an array
+		*/
+		int i;
+		String[] cars= {"bmw","volvo","mazda","subaru","cadilac"};
 		
-		//String i;
-		String[] cars= {"bmw","volvo","mazda","subaru"};
+		/*
 		for(String i:cars){
 			
 			System.out.println(i);
@@ -28,7 +31,122 @@ public class SampleClass {
 		}
 		
 		
-
+		
+		for(i=0;i<cars.length;i++)
+		{
+			System.out.println(cars[i]);
+		}
+		*/
+		
+		
+	/*
+		//access multidimensional array
+		int[][] numbers= {{1,2,3,4,5},{1,2,3,4,5},{1}};
+		
+		int x=numbers[1][3];
+		//System.out.println("my output is :"+x);
+		
+		System.out.println(numbers[1].length);
+		
+	*/
+	
+		
+		/*
+		
+	int firstarray[][]= {{1,2,3,4,5},{33},{4,5,6}};
+	int secondarray[][]= {{1,2,3},{3,4,5,6},{8,9,10}};
+	
+	System.out.println("This is first array");
+	
+	display(firstarray);
+	
+	System.out.println("This is second array");
+	
+	display(secondarray);
+   public	static void display(int x[][])
+	{
+	 
+	   for(int row=0;row<x.length;row++)
+	   {
+		   for(int col=0;col<x[row].length;col++)
+		   {
+			  System.out.print(x[row][col]+"\t"); 
+			   
+		   }
+		   System.out.println();
+	   }   
+	   
 	}
+	
+	*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		 
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
 
 }
