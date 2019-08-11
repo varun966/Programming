12ed811341b76fa53f8b3bf0b1a433f76d441b20
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-08-11 14:15:46 -0500

    w3

---
+++ javaprogramming/DemoProject/src/Newclass.java
@@ -0,0 +1,27 @@
+
+
+public class Newclass {
+
+	
+	int distance;
+	
+	
+	public Newclass(int value)
+	{
+		distance=value;
+	}
+	
+	public static void main(String[] args) {
+	
+		
+		Newclass Obj1 = new  Newclass(2);
+		
+		//Obj1.x=10;
+		System.out.println(Obj1.distance);
+		
+		
+		
+		
+	}
+
+}
